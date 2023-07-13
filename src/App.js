e326fada3d8b9784088f416b17115bed51a03f19
Andy Loredo <ndy.loredo@gmail.com>
2023-07-13 10:17:05 -0400

    add a form with styles, as well as event handling logics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Expenses from "./components/expenses/Expenses";
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   return (
     <div>
+      <NewExpense />
       <Expenses />
     </div>
   );
-}
+};
 
 export default App;
