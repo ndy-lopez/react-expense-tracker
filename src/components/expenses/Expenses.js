6e1f6aeab71bb0c3fd613c37d043d8652d05e4aa
Andy Loredo <ndy.loredo@gmail.com>
2023-07-14 10:15:08 -0400

    table now works by adding new content and updates to list

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -6,37 +6,23 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHandler}/>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
