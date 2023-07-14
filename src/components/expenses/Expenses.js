35ae390827b2a9f03e63486219cdfa91e37152f9
Andy Loredo <ndy.loredo@gmail.com>
2023-07-14 10:29:55 -0400

    added key to values and set filtered by year

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -12,17 +12,25 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
-        ))}
+        ))};
       </Card>
     </div>
   );
